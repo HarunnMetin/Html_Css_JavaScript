aa9c6e384f50216f0f77305d9fc9b1caa4a468bf
HarunnMetin <mtnmusa07@gmail.com>
2025-03-19 03:07:30 +0300

    alert

--- JavaScript/5-Javascript/2-cikti-verme/app.js
+++ JavaScript/5-Javascript/2-cikti-verme/app.js
@@ -1,1 +1,28 @@
-//* document write ile cikti verme
+const p = document.querySelector("p");
+p.innerText = "hello";
+
+document.writeln("hello 2");
+document.write("Thats easy");
+
+console.log("clear");
+console.log("clear");
+console.log("clear");
+
+let a = 45;
+let b = 15;
+
+setTimeout(() => {
+  if (a / b === 3) {
+    console.log("buyuktur");
+  } else {
+    console.clear();
+  }
+}, 2000);
+
+console.log(window);
+console.log(document.location.host);
+
+alert("hosgeldiniz");
+setTimeout(() => {
+  alert("iki sayinin toplami:" + (a + b));
+}, 1000);
