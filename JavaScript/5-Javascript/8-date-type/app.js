60f893d852cded031a7d92c63536e6be7e255655
HarunnMetin <mtnmusa07@gmail.com>
2025-03-26 02:01:10 +0300

    date-type

---
+++ JavaScript/5-Javascript/8-date-type/app.js
@@ -0,0 +1,38 @@
+// let a = 5;
+// let b = "20";
+
+// console.log(a+b);
+
+// let c = Number(b);
+
+// console.log(a+c);
+
+// let d = a.toString();
+
+// console.log(c+d);
+
+let a = 10;
+let b = "2";
+
+console.log(a + b);
+
+let c = parseInt(b);
+
+console.log(a+c);
+
+let sayilar = [1,2,3,4,5];
+
+let changeToString = String(sayilar);
+
+console.log(typeof changeToString);
+console.log(changeToString);
+
+let sayilar2 = [56,34,23,12,34,54,10];
+let useToString = sayilar2.toString();
+console.log(typeof useToString);
+console.log(useToString);
+
+ 
+
+
+
