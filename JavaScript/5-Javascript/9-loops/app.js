644d9bcc3f2130b66660d08beb63765988030dbd
HarunnMetin <mtnmusa07@gmail.com>
2025-03-26 03:44:31 +0300

    for-loop

---
+++ JavaScript/5-Javascript/9-loops/app.js
@@ -0,0 +1,23 @@
+// cift sayilari yazdir
+
+for (let i = 0; i <= 10; i += 2) {
+  console.log(i);
+}
+
+// tek sayilari yazdir
+for (let i = 1; i <= 10; i += 2) {
+  console.log(i);
+}
+
+for(let i = 1; i<=10; i++){
+
+    if(i%2==1){
+        console.log("Hello");
+        
+    }
+    else{
+        console.log("World");
+        
+    }
+
+}
