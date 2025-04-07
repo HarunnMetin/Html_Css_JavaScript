c29f0fe3365fbf7560b8b48b2abbbf03500a8186
HarunnMetin <mtnmusa07@gmail.com>
2025-04-07 20:32:59 +0300

    method

--- JavaScript/5-Javascript/13-function/app.js
+++ JavaScript/5-Javascript/13-function/app.js
@@ -1,2 +1,14 @@
-//? PARAMETRESİZ VE GERİYE DEGER DONDURMEYEN METOT TANIMLAMAK
+//? PARAMETRELI METHOD
+
+let isim = prompt();
+let soyisim = prompt();
+
+yazdir(isim,soyisim);
+
+
+function yazdir(isim,soyisim){
+
+    console.log(isim + " " + soyisim);
+    
+}
 
