a1df8548955f5a97c4e26972341fee702bea8ba1
HarunnMetin <mtnmusa07@gmail.com>
2025-04-10 00:20:02 +0300

    Math

---
+++ JavaScript/5-Javascript/17-math/app.js
@@ -0,0 +1,32 @@
+console.log(window.Math);
+
+
+let a = 3.55;
+
+console.log(Math.floor(a));
+
+console.log(Math.ceil(a));
+
+console.log(Math.round(a));
+
+
+let arr = [-1,3,5,34,53,64];
+
+console.log(Math.max(...arr));
+
+console.log(Math.min(...arr));
+
+console.log(Math.abs(Math.min(...arr)));
+
+console.log(Math.sqrt(144));
+
+console.log(Math.pow(2,2));
+
+console.log(Math.floor(Math.random() * 10000));
+
+
+
+
+
+
+
