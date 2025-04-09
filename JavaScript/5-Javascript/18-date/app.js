2569e7c49af42f0b870b67761d062a62388b9e2c
HarunnMetin <mtnmusa07@gmail.com>
2025-04-10 01:28:16 +0300

    date

---
+++ JavaScript/5-Javascript/18-date/app.js
@@ -0,0 +1,21 @@
+let tarih = new Date();
+
+console.log(tarih);
+
+console.log(tarih.getFullYear());
+
+console.log(tarih.getDate());
+
+console.log(tarih.getDay());
+
+console.log(tarih.getMilliseconds());
+
+let a = "1";
+
+console.log(tarih.getHours() + a.toString());
+
+console.log(tarih.toLocaleDateString());
+
+console.log(tarih.toLocaleTimeString());
+
+console.log(tarih.toLocaleString());
