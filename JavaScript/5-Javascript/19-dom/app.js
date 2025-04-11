3a045b3b270ff0a2fb727f2e87129deee4396bd9
HarunnMetin <mtnmusa07@gmail.com>
2025-04-11 22:03:01 +0300

    dom

---
+++ JavaScript/5-Javascript/19-dom/app.js
@@ -0,0 +1,14 @@
+console.log(window.document.location.href);
+console.log(window.document.location.hostname);
+
+let deger = window.document.location.pathname;
+
+deger = document.all;
+
+deger = document.title;
+
+deger = document.links;
+deger = document.links[1];
+deger = document.links.item[0];
+
+console.log(deger);
