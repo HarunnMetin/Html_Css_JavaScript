bd1b0f611fadac972cc88fb5a56e75cd31cf5da7
HarunnMetin <mtnmusa07@gmail.com>
2025-04-13 01:01:19 +0300

    style

---
+++ JavaScript/5-Javascript/23-javascript-style/app.js
@@ -0,0 +1,35 @@
+// const body = document.querySelector("body");
+
+// body.style.display = "flex";
+// body.style.justifyContent = "center";
+// body.style.alignItems = "center";
+
+const card = document.querySelector("#firstCard .title");
+
+card.style.color = "red";
+card.style.fontSize = "90px";
+card.style.display = "flex";
+card.style.justifyContent = "center";
+card.style.fontWeight = "bolder";
+
+const content = document.querySelector("#firstCard .content");
+
+content.style.borderTop = "30px";
+content.style.fontSize = "50px";
+content.style.backgroundColor = "#102E50";
+content.style.color = "#fff";
+content.style.display = "flex";
+content.style.justifyContent = "center";
+content.style.alignContent = "center";
+content.style.fontWeight ="bold";
+content.style.paddingLeft = "40px";
+content.style.marginTop = "80px";
+
+const button = document.querySelector("#changeButton");
+
+button.style.border = "none";
+button.style.marginTop = "30px"
+button.style.marginLeft = "50px";
+button.style.border = "2px solid #000";
+button.style.borderRadius = "30px";
+
