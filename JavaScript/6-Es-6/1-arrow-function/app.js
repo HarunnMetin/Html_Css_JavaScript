e98851d9db3c4992ac7a26d1795e833c4515ba8e
HarunnMetin <mtnmusa07@gmail.com>
2025-04-23 03:40:46 +0300

    arrow-function

---
+++ JavaScript/6-Es-6/1-arrow-function/app.js
@@ -0,0 +1,21 @@
+//*ARROW FUNCTION
+
+const yazdir = () => {
+  console.log("Hello");
+};
+
+yazdir();
+
+const firstName = (firstName) => {
+  console.log("merhaba", firstName);
+};
+
+firstName("eart");
+
+const age = (age) => console.log("Benim yasim", age);
+age(24);
+const kupAl = (x) => {
+  return x * x * x;
+};
+
+console.log("Deger :", kupAl(3));
