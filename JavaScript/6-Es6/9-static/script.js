060536bf391ece4eaecb4260d091d60eda4b1a09
HarunnMetin <mtnmusa07@gmail.com>
2025-04-29 03:21:06 +0300

    static

---
+++ JavaScript/6-Es6/9-static/script.js
@@ -0,0 +1,19 @@
+class Matematik {
+  static topla(a, b) {
+    console.log(a + b);
+  }
+  cikar(a, b) {
+    console.log(a - b);
+  }
+  carp(a, b) {
+    console.log(a * b);
+  }
+  bol(a, b) {
+    console.log(a / b);
+  }
+}
+
+const matematik = new Matematik();
+
+Matematik.topla(2,2);
+matematik.cikar(8,9);
