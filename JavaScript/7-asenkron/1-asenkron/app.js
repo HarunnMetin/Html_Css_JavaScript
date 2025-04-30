aca2a82bc9c8a7f2eb15621dc550a53805e0437d
HarunnMetin <mtnmusa07@gmail.com>
2025-05-01 01:15:00 +0300

    asenkron

---
+++ JavaScript/7-asenkron/1-asenkron/app.js
@@ -0,0 +1,13 @@
+console.log("Harun");
+
+setTimeout(() => {
+  console.log("bir saniye doldu ve calisti");
+},2000);
+
+setTimeout(()=>{
+    console.log("yarim saniye bekledi ve calisti");
+    
+},500)
+
+console.log("Metin");
+
