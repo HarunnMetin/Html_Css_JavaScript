b3ba4907a83c65173a2046533993b3cee4141348
HarunnMetin <mtnmusa07@gmail.com>
2025-12-26 22:59:23 +0300

    loop

---
+++ JavaScript/JavaScript-December-25/15.loop-example/loop..js
@@ -0,0 +1,11 @@
+let num = 0;
+let sum = 0;
+
+while (num < 1000) {
+  num++;
+  sum += num;
+}
+
+console.log("Toplanan sayi toplami : " + num);
+console.log("Toplam : " + sum);
+console.log("Son eklenen: " + num);
