167068c92df0da1780b5595570d70bd59b9452e4
HarunnMetin <mtnmusa07@gmail.com>
2025-12-27 01:51:41 +0300

    loop

---
+++ JavaScript/JavaScript-December-25/17.loop-example/loop..js
@@ -0,0 +1,20 @@
+for (let i = 1; i <= 5; i++) {
+  let line = "";
+  for (let j = 1; j <= i; j++) {
+    line += i;
+  }
+  console.log(line);
+}
+
+console.log("******************");
+
+for (let i = 5; i >= 1; i--) {
+  let line = "";
+  for (let j = 1; j <= i; j++) {
+    line += i;
+    
+  }
+  console.log(line);
+  
+  
+}
