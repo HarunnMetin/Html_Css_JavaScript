e3e1a27aee2bbc473cb6bcbbc3f48731c21fe9bf
HarunnMetin <mtnmusa07@gmail.com>
2026-01-02 23:27:04 +0300

    arr

--- JavaScript/JavaScript-December-25/25.array/array.js
+++ JavaScript/JavaScript-December-25/25.array/array.js
@@ -10,6 +10,20 @@ prices.shift();
 
 console.log(prices);
 
+console.log(prices.includes(200));
 
+console.log(prices.indexOf(45));
+
+let newPriceArr = prices.slice(0, 4);
+
+console.log(newPriceArr);
+
+let raiseNewPrice = [];
+for (let i = 0; i < prices.length; i++) {
+  let increase = prices[i] + prices[i]*(10/100);
+  raiseNewPrice.push(increase)
+  
+}
+console.log(raiseNewPrice);
 
 
