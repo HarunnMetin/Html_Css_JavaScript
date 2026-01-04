0d79984c858423c9d3f30535609f11299ee847f0
HarunnMetin <mtnmusa07@gmail.com>
2026-01-04 23:55:43 +0300

    reduce

---
+++ JavaScript/JavaScript-December-25/35.reduce/reduce.js
@@ -0,0 +1,35 @@
+/*
+Banlı olmayan
+
+Level ≥ 10
+
+En yüksek level’a sahip oyuncuyu bul
+
+Yoksa → null
+*/
+
+const players = [
+  { username: "Shadow", level: 12, banned: false },
+  { username: "RogueX", level: 25, banned: true },
+  { username: "Knight", level: 18, banned: false },
+  { username: "Noob", level: 9, banned: false },
+];
+
+let noBnnedPlayer = players.reduce((find, player) => {
+  if (player.banned === true) {
+    return find;
+  }
+  if (player.level >= 10) {
+    return find;
+  }
+  if (find === null) {
+    return player;
+  }
+  if (player.level > find.level) {
+    return player;
+  }
+
+  return find;
+}, null);
+
+console.log(noBnnedPlayer);
