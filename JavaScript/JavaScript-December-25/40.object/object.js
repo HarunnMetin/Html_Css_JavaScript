559e35a8d96fe3ab70a2176dd52af7bbf49014f6
HarunnMetin <mtnmusa07@gmail.com>
2026-01-07 16:02:37 +0300

    object

---
+++ JavaScript/JavaScript-December-25/40.object/object.js
@@ -0,0 +1,36 @@
+const shop = {
+  name: "Tech Store",
+  products: [
+    { name: "Phone", price: 1000, inStock: true },
+    { name: "Headset", price: 200, inStock: false },
+    { name: "Charger", price: 50, inStock: true },
+  ],
+  owner: {
+    name: "Ali",
+    isActive: true,
+  },
+};
+
+//Mağaza sahibinin adını console’a yazdır.
+
+console.log(`Magaza sahibi adi :${shop.owner.name}`);
+
+//Stokta olmayan en az bir ürün var mı?
+
+let stockControl = shop.products.some((product) => product.inStock === false);
+if (stockControl) {
+  console.log(`Stokta olmayan en az bir urun var`);
+}
+
+//Stokta olan ürünlerin toplam fiyatını hesapla.
+
+let productCount = 0;
+
+let stock = shop.products.reduce((acc, product) => {
+  if (product.inStock) {
+    acc += product.price;
+  }
+  return acc;
+}, 0);
+
+console.log(`Stokta olan ürünlerin toplam: ${stock}`);
